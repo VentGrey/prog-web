9de26e7200d04199efd510dd0f82b43fd64d4f19
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-09-12 20:07:06 -0500

    New VS project

---
+++ Programación Web/Prácticas/Tienda/Tienda/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Tienda.Models
+{
+    public class Producto {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public float Precio { get; set; }
+        public string Imagen { get; set; }
+        public string Marca { get; set; }
+    }
+}
